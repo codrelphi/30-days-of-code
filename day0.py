69a31a09bf262344e33964b732bed705c809b3b5
codrelphi <chancerel.codjovi@gmail.com>
2019-09-04 23:44:49 +0100

    add a header (author, date, source)

--- day0.py
+++ day0.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
+#==============================================================
+#author: Chancerel Codjovi (aka codrelphi)
+#date: 2019-09-04        
+#source: hackerrank.com/challenges/30-hello-world/problem    
+#==============================================================
 
 """
 Sample Input
