037b049cdfa3708d867e47afdde6fe048e9bc9ea
codrelphi <chancerel.codjovi@gmail.com>
2019-09-08 21:08:20 +0100

    Add my personal header (author, date, etc.) in the file!

---
+++ day4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+#====================================================================
+#author: Chancerel Codjovi (aka codrelphi)
+#date: 2019-09-08
+#source: https://www.hackerrank.com/challenges/30-class-vs-instance/problem
+#=====================================================================
