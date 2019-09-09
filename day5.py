9913ee07de62aefe842adf22c5534f0b2aaaf426
codrelphi <chancerel.codjovi@gmail.com>
2019-09-09 09:25:50 +0100

    create the script for <Day 5: Loops> challenge!

--- day5.py
+++ day5.py
@@ -5,3 +5,19 @@
 #date: 2019-09-09
 #source: https://www.hackerrank.com/challenges/30-loops/problem
 #=====================================================================
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    while n < 2 or n > 20:
+        print("Erreur: 2 <= n <= 20. Réessayez!")
+        n = int(input())
+    for i in range(1, 11):
+        result = n * i
+        print("{} x {} = {}".format(n, i, result))
