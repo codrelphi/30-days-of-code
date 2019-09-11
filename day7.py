ccc37f9175817f9e21f48fa8a38917e4c7089faf
codrelphi <chancerel.codjovi@gmail.com>
2019-09-11 09:26:02 +0100

    create the script for <Day 7: Arrays> challenge!

--- day7.py
+++ day7.py
@@ -5,3 +5,25 @@
 #date: 2019-09-11
 #source: https://www.hackerrank.com/challenges/30-arrays/problem
 #=====================================================================
+
+
+if __name__ == "__main__":
+    n = int(input())
+    a = []
+    if n < 1 or n > 1000:
+        print("Error !")
+        exit()
+    else:
+        input = input()
+        a = input.split(" ")
+        if len(a) == n:
+            a = [int(i) for i in a]
+        else:
+            print("Error !")
+            exit()
+
+    a.reverse()
+    # print(*a) => first method of printing
+    # 2nd method of printing
+    for i in a:
+        print(i, end=" ")
