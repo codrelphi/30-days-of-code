dac212736cc28bae610b545a8771429611e6d534
codrelphi <chancerel.codjovi@gmail.com>
2019-09-12 12:54:04 +0100

    create the script for <Day 8: Dictionaries and Maps> challenge!

--- day8.py
+++ day8.py
@@ -5,3 +5,31 @@
 #date: 2019-09-12
 #source: https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
 #=====================================================================
+import sys
+
+if __name__ == "__main__":
+    phoneBook = {}
+    queries = []
+    n = int(input())
+    if n < 1 or n > 100000:
+        print("Error !")
+        exit()
+
+    for i in range(n):
+        entry_split = input().split(sep=" ")
+        name = entry_split[0].lower()
+        phoneNumber = entry_split[1]
+        if len(phoneNumber) != 8:
+            print("Error !")
+            exit()
+        phoneBook[name] = phoneNumber
+
+    for i in range(n):
+        try:
+            query = input()
+            if query in phoneBook:
+                print("{}={}".format(query, phoneBook[query]))
+            else:
+                print("Not found")
+        except EOFError as error:
+            break
