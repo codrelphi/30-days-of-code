afe8f7f28f209400a46293d9ea5e38695d9ade4e
codrelphi <chancerel.codjovi@gmail.com>
2019-09-13 17:30:31 +0100

    create the script for <Day 9: Recursion 3> challenge!

--- day9.py
+++ day9.py
@@ -5,3 +5,21 @@
 # date: 2019-09-13
 # source: https://www.hackerrank.com/challenges/30-recursion/problem
 #=================================================================================
+import sys
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# to test the factorial function
+if __name__ == "__main__":
+    n = int(input())
+    if n < 0:
+        print("Error: enter a positive integer !")
+        exit()
+    if n > 12:
+        print("Error: the number should be <= 12 !")
+        exit()
+    print(factorial(n))
