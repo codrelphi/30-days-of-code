1eed59302db165169616d31736f487df4b2cf93c
codrelphi <chancerel.codjovi@gmail.com>
2019-09-15 23:46:24 +0100

    modify the date of my personal header

--- day11.py
+++ day11.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 #=================================================================================
 # author: Chancerel Codjovi (aka codrelphi)
-# date: 2019-09-14
+# date: 2019-09-15
 # source: https://www.hackerrank.com/challenges/30-2d-arrays/problem
 #=================================================================================
