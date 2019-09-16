327501d2f70c659aa001693c014631d706ed39ca
codrelphi <chancerel.codjovi@gmail.com>
2019-09-16 14:42:09 +0100

    create the script for <Day 12: Inheritance> challenge!

--- day12.py
+++ day12.py
@@ -5,3 +5,57 @@
 # date: 2019-09-16
 # source: https://www.hackerrank.com/challenges/30-inheritance/problem
 #=================================================================================
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+
+class Student(Person):
+    #   Class Constructor
+    #
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self, firstName, lastName, id, scores):
+        super().__init__(firstName, lastName, id)
+        self.scores = scores
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        grade = ''
+        a = sum(self.scores) / len(self.scores)
+        if a < 40:
+            grade = 'T'
+        if 40 <= a and a < 55:
+            grade = 'D'
+        if 55 <= a and a < 70:
+            grade = 'P'
+        if 70 <= a < 80:
+            grade = 'A'
+        if 80 <= a < 90:
+            grade = 'E'
+        if 90 <= a <= 100:
+            grade = 'O'
+        return grade
+        
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
