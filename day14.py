b0f726633183457d638d0921af3e2dfdc331286a
codrelphi <chancerel.codjovi@gmail.com>
2019-09-18 11:52:24 +0100

    create the script for <Day 14: Scope> challenge using ValueError exception!

--- day14.py
+++ day14.py
@@ -5,3 +5,23 @@
 # date: 2019-09-18
 # source: https://www.hackerrank.com/challenges/30-scope/problem
 #=================================================================================
+
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+        self.maximumDifference = 0
+
+    def computeDifference(self):
+        try:
+            self.maximumDifference = max([abs(i-j) for i in self.__elements for j in self.__elements if i!=j])
+        except ValueError:
+            self.maximumDifference = 0
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
