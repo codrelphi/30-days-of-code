1f435b602e29477298cbd413d42629bd73977d7b
codrelphi <chancerel.codjovi@gmail.com>
2019-09-30 12:48:06 +0100

    update: add the real coding date

--- day15.py
+++ day15.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #=================================================================================
 # author: Chancerel Codjovi (aka codrelphi)
-# date: 2019-09-19
+# date (file creation): 2019-09-19
+# date (coding): 2019-09-30
 # source: https://www.hackerrank.com/challenges/30-linked-list/problem
 #=================================================================================
