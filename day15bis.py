55f4a66f600e3ae542c3fa28fdfaf827d27206f1
codrelphi <chancerel.codjovi@gmail.com>
2019-10-01 11:17:44 +0100

    write the class Node

--- day15bis.py
+++ day15bis.py
@@ -5,3 +5,10 @@
 # date: 2019-10-01
 # source: https://www.hackerrank.com/challenges/30-linked-list/problem
 #=================================================================================
+
+# Day 15: a new version write from begin to end by myself
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
