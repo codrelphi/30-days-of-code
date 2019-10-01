fedff150aa4f596218d3d5cf2a0581972de00f5e
codrelphi <chancerel.codjovi@gmail.com>
2019-10-01 11:20:10 +0100

    update my personal header

--- day16.py
+++ day16.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #=================================================================================
 # author: Chancerel Codjovi (aka codrelphi)
-# date: 2019-09-20
+# date (file creation): 2019-09-20
+# date (coding): 2019-10-01
 # source: https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
 #=================================================================================
