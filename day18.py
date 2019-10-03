aac43d4d63b903c14fde8ab3340b3f8adb1a7ca5
codrelphi <chancerel.codjovi@gmail.com>
2019-10-03 13:56:09 +0100

    write the script for <Day 18: Queues and Stacks> challenge!

--- day18.py
+++ day18.py
@@ -5,3 +5,52 @@
 # date: 2019-10-03
 # source: https://www.hackerrank.com/challenges/30-more-exceptions/problem
 #=================================================================================
+import sys
+
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.queue = []
+
+    def pushCharacter(self, ch):
+        """ pushes the character ch onto the stack """
+        self.stack.append(ch)
+
+    def enqueueCharacter(self, ch):
+        """ enqueues the character ch in the queue """
+        self.queue.append(ch)
+
+    def popCharacter(self):
+        """ pops and returns the character at the top of the stack """
+        return self.stack.pop()
+
+    def dequeueCharacter(self):
+        """ dequeues and returns the first character in the queue """
+        return self.queue.pop(0)
+
+# read the string s
+s=input()
+#Create the Solution class object
+obj=Solution()
+
+l=len(s)
+# push/enqueue all the characters of string s to stack
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+
+isPalindrome=True
+'''
+pop the top character from stack
+dequeue the first character from queue
+compare both the characters
+'''
+for i in range(l // 2):
+    if obj.popCharacter()!=obj.dequeueCharacter():
+        isPalindrome=False
+        break
+#finally print whether string s is palindrome or not.
+if isPalindrome:
+    print("The word, "+s+", is a palindrome.")
+else:
+    print("The word, "+s+", is not a palindrome.")    
