b45727d08a447ca64c722fc04dd5c1ac6fa563bd
codrelphi <chancerel.codjovi@gmail.com>
2019-10-05 02:31:31 +0100

    write the code for <Day 20: Sorting> challenge!

--- day20.py
+++ day20.py
@@ -5,3 +5,23 @@
 # date: 2019-10-05
 # source: https://www.hackerrank.com/challenges/30-sorting/problem
 #=================================================================================
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    a = list(map(int, input().strip().split(' ')))
+
+    numberOfSwaps = 0
+    for i in range(n):
+        for j in range(n-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+                numberOfSwaps += 1
+
+        if numberOfSwaps == 0:
+            break
+
+    print("Array is sorted in {} swaps.".format(numberOfSwaps))
+    print("First Element:", a[0])
+    print("Last Element:", a[n-1])
