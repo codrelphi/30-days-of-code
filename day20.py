11f3603f0648c48aed8c431fcfc90b90bb598055
codrelphi <chancerel.codjovi@gmail.com>
2019-10-05 02:30:29 +0100

    create the file day20.py and add my personal header

---
+++ day20.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-05
+# source: https://www.hackerrank.com/challenges/30-sorting/problem
+#=================================================================================
