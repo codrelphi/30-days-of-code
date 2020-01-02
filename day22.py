936a3733fdaac168355a9e662e9791ab8e360ab7
codrelphi <chancerel.codjovi@gmail.com>
2020-01-02 09:29:51 +0100

    update the date

--- day22.py
+++ day22.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #=================================================================================
 # author: Chancerel Codjovi (aka codrelphi)
-# date: 2019-10-06
+# date (file creation): 2019-10-06
+# date (code written): 2020-01-02
 # source: https://www.hackerrank.com/challenges/30-binary-search-trees/problem
 #=================================================================================
