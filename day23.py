e8ca9b05f3386e2f2d2f2d50d7484044e0dffa76
codrelphi <chancerel.codjovi@gmail.com>
2020-01-03 08:26:29 +0100

    add my personal header

---
+++ day23.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2020-01-03
+# source: https://www.hackerrank.com/challenges/30-binary-trees/problem
+#=================================================================================
