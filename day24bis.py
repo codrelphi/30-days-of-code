1045fe4b92005b9d39967123f567aa30097660b6
codrelphi <chancerel.codjovi@gmail.com>
2020-01-04 16:31:39 +0100

    use a simple way to solve the problem. This algo compares two adjacents values.

--- day24bis.py
+++ day24bis.py
@@ -5,3 +5,44 @@
 # date: 2020-01-04
 # source: https://www.hackerrank.com/challenges/30-linked-list-deletion/problem
 #=================================================================================
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def insert(self,head,data):
+            p = Node(data)
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        new_head = head
+        while head.next is not None:
+            if head.next.data == head.data:
+                head.next = head.next.next
+            else:
+                head = head.next
+        return new_head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+head=mylist.removeDuplicates(head)
+mylist.display(head);
