488441b5e9fee8517770e84ffc4f593b16bc391a
codrelphi <chancerel.codjovi@gmail.com>
2020-01-05 16:01:50 +0100

    write the working code. 2/10 test cases failed

--- day25.py
+++ day25.py
@@ -5,3 +5,21 @@
 # date: 2020-01-05
 # source: https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
 #=================================================================================
+
+def prime_or_not(n):
+    count = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            count += 1
+    if count == 2:
+        print("Prime")
+    else:
+        print("Not prime")
+
+liste = list()
+n = int(input())
+for i in range(n):
+    liste.append(int(input()))
+
+for i in liste:
+    prime_or_not(i)
