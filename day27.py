9bbb5c4bf08180ad2a0b8421d6f00b46952e7ea9
codrelphi <chancerel.codjovi@gmail.com>
2020-01-07 17:29:52 +0100

    add my personal header

---
+++ day27.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2020-01-07
+# source: https://www.hackerrank.com/challenges/30-testing/problem
+#=================================================================================
