3dcd3437abf0f3851ebaf5243c42d5915f7e53a7
codrelphi <chancerel.codjovi@gmail.com>
2020-01-10 23:06:24 +0100

    add my personal header

---
+++ day29.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2020-01-10
+# source: https://www.hackerrank.com/challenges/30-bitwise-and/problem
+#=================================================================================
