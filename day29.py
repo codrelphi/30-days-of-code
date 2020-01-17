3fd853f95ca1408ca61ef467abd8a2b7ca16cec6
codrelphi <chancerel.codjovi@gmail.com>
2020-01-17 23:59:36 +0100

    update the date

--- day29.py
+++ day29.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 #=================================================================================
 # author: Chancerel Codjovi (aka codrelphi)
-# date: 2020-01-16
+# date: 2020-01-17
 # source: https://www.hackerrank.com/challenges/30-bitwise-and/problem
 #=================================================================================
